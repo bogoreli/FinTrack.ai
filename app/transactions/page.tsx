597c3fdefca09cdfa3712ef03b3011f421af7526
Kevin Bogoreli <bogoreli.kevin@outlook.com>
2026-01-29 13:57:31 -0300

    feat: ensure transactions and subscription pages are protected

--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -3,9 +3,18 @@ import { DataTable } from "../_components/ui/data-table";
 import { transactionColumns } from "./_columns";
 import AddTransactionButton from "../_components/add-transaction";
 import Navbar from "../_components/navbar";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
 const Transaction = async () => {
-  const transactions = await db.transaction.findMany({});
+  const { userId } = await auth();
+  if (!userId) {
+    redirect("/login");
+  }
+  const transactions = await db.transaction.findMany({
+    where: { userId },
+    orderBy: { createdAt: "desc" },
+  });
   return (
     <>
       <Navbar />
