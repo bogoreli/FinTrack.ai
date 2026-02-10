56afef4962ad7d260af81cf45c9b6b4d70e3c8df
Kevin Bogoreli <bogoreli.kevin@outlook.com>
2026-02-10 15:50:41 -0300

    Revert "fix: fix issues during the build"
    This reverts commit 66211382a868df1a4a35ce2ef3e2d69c41f6f153.

--- app/(home)/_actions/generate-ai-report/index.ts
+++ app/(home)/_actions/generate-ai-report/index.ts
@@ -5,13 +5,11 @@ import { auth, clerkClient } from "@clerk/nextjs/server";
 import { startOfMonth, endOfMonth } from "date-fns";
 import OpenAI from "openai";
 import { GenerateAiReportInputSchema, generateAiReportSchema } from "./schema";
-import { Transaction } from "@prisma/client";
 
 export const generateAiReport = async ({
   month,
 }: GenerateAiReportInputSchema) => {
   generateAiReportSchema.parse({ month });
-
   const { userId } = await auth();
   if (!userId) {
     throw new Error("Unauthorized");
@@ -50,14 +48,12 @@ export const generateAiReport = async ({
   });
 
   const content = `Gere um relatório com insights sobre as minhas finanças, com dicas e orientações de como melhorar minha vida financeira. As transações estão divididas por ponto e vírgula. A estrutura de cada uma é {DATA}-{TIPO}-{VALOR}-{CATEGORIA}. São elas:
-${transactions
-  .map(
-    (transaction: Transaction) =>
-      `${transaction.createdAt.toLocaleDateString(
-        "pt-BR",
-      )}-R$${transaction.amount}-${transaction.type}-${transaction.category}`,
-  )
-  .join(";")}`;
+  ${transactions
+    .map(
+      (transaction) =>
+        `${transaction.date.toLocaleDateString("pt-BR")}-R$${transaction.amount}-${transaction.type}-${transaction.category}`,
+    )
+    .join(";")}`;
 
   const completion = await openAi.chat.completions.create({
     model: "gpt-4o-mini",
