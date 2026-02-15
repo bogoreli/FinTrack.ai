9c1227c793a043aae6e628a4516c3625163460f7
Kevin Bogoreli <bogoreli.kevin@outlook.com>
2026-02-15 11:05:23 -0300

    fix: subscription page

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { auth, clerkClient } from "@clerk/nextjs/server";
 import Navbar from "../_components/navbar";
 import { redirect } from "next/navigation";
